1db10bd5f92935d36aba912407d9dd447b1b97c7
Jayden <linjie@fireworkhq.com>
2022-04-23 00:40:28 +0800

    Update SDK to v1.0.0

--- FireworkFlutterSDKSample/lib/main.dart
+++ FireworkFlutterSDKSample/lib/main.dart
@@ -1,15 +1,8 @@
-import 'dart:io';
-
-import 'package:fw_flutter_sdk/fw_flutter_sdk.dart';
 import 'package:fw_flutter_sdk_example/utils/fw_example_logger_util.dart';
 import 'package:flutter/material.dart';
 import 'my_app.dart';
 
 void main() {
   FWExampleLoggerUtil.log("main: run app");
-  if (Platform.isAndroid) {
-    // Enable hybrid composition
-    VideoFeed.useHybridCompositionForAndroid = true;
-  }
   runApp(const MyApp());
 }
