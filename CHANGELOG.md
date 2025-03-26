62398c59287ac1b516d1962ec2a4205cfebeae90
Jayden <linjie@fireworkhq.com>
2025-03-26 09:49:21 +0000

    Update sample codes for v2.15.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # CHANGELOG
 
+## [2.15.1]
+
+### Fixed
+
+- Bug fixes and improvements
+
 ## [2.15.0]
 
 ### Added
--- FireworkFlutterSDKSample/lib/screens/more/more_screen.dart
+++ FireworkFlutterSDKSample/lib/screens/more/more_screen.dart
@@ -7,7 +7,7 @@ import 'package:fw_flutter_sdk_example/utils/host_app_service.dart';
 import '../../generated/l10n.dart';
 import '../../widgets/fw_app_bar.dart';
 
-const fwNativeVersionOfAndroid = '6.18.0';
+const fwNativeVersionOfAndroid = '6.18.1';
 
 class MoreScreen extends StatefulWidget {
   const MoreScreen({
