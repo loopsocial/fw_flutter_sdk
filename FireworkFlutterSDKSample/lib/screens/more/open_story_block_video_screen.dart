1340b47ec88b58880dce474eba3fb044e8810e07
Jayden <linjie@fireworkhq.com>
2025-09-15 00:00:45 +0000

    Update sample codes for v2.20.2-beta.1

--- FireworkFlutterSDKSample/lib/screens/more/open_story_block_video_screen.dart
+++ FireworkFlutterSDKSample/lib/screens/more/open_story_block_video_screen.dart
@@ -140,6 +140,7 @@ class _OpenStoryBlockVideoScreenState extends State<OpenStoryBlockVideoScreen> {
                   borderRadius: BorderRadius.circular(20),
                   boxShadow: [
                     BoxShadow(
+                      // ignore: deprecated_member_use
                       color: Colors.black.withOpacity(0.1),
                       blurRadius: 10,
                       offset: const Offset(0, 5),
